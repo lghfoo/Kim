81d152d73afb290dfbd37015fb7b171978edbf7f
lghfoo <1162820772@qq.com>
2020-01-16 22:49:51 +0800

    update connection view

--- view/ConnectionView.hpp
+++ view/ConnectionView.hpp
@@ -1,11 +1,16 @@
 #pragma once
 #include<QGraphicsItem>
+#include <QPainter>
 namespace Kim {
     class KConnectionView : public QGraphicsItem{
     public:
         enum EShapeType{Line, Quad, Cubic};
     private:
         EShapeType ShapeType;
+        QPointF From;
+        QPointF CtrlFrom;
+        QPointF CtrlTo;
+        QPointF To;
     public:
         void SetShapeType(EShapeType ShapeType){
             switch (ShapeType) {
@@ -20,7 +25,24 @@ namespace Kim {
             }
         }
         virtual QRectF boundingRect() const = 0;
-        virtual void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget = 0) = 0;
+        virtual void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget = 0) override{
+            QPainterPath Path;
+            Path.moveTo(From);
+            switch (ShapeType) {
+            case EShapeType::Line:
+                Path.lineTo(To);
+                break;
+            case EShapeType::Quad:
+                Path.quadTo(CtrlFrom, To);
+                break;
+            case EShapeType::Cubic:
+                Path.cubicTo(CtrlFrom, CtrlTo, To);
+                break;
+            default:
+                break;
+            }
+            painter->drawPath(Path);
+        }
 
     };
 }
